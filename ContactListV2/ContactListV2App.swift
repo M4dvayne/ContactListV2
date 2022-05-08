a3a87f5c2aef1d0b246572c531d876fb6ee3cc06
KremnevViacheslav <rusted2192@gmail.com>
2022-05-08 23:03:47 +0300

    Initial Commit

---
+++ ContactListV2/ContactListV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListV2App.swift
+//  ContactListV2
+//
+//  Created by Вячеслав Кремнев on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
